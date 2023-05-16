34f7e4054d40b16b43be33948badb97e02cc8bae
Andrea Gobbi <agobbi@fbk.eu>
2023-05-16 15:38:43 +0200

    Initial commit

---
+++ init.sh
@@ -0,0 +1,10 @@
+conda create -n incube
+conda activate incube
+conda install pip
+pip install jupyter notebook
+pip install torch
+pip install pytorch-lightning
+pip install plotly
+	pip install matplotlib
+#andare nella lib dsipts
+python setup.py install --force
